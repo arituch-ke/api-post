477f23076c22f434d8d53b3b8710c75d533c79b8
anon-kae <anon.ksl42@gmail.com>
2024-06-01 16:40:56 +0700

    feat: Add auth, user, and auth routes for login, token refresh, and user-related functionality

--- src/transports/http/routes/index.ts
+++ src/transports/http/routes/index.ts
@@ -1,8 +1,12 @@
 import {Router} from 'express';
 import rootRouter from './root';
+import authRouter from './auth';
+import userRouter from './user';
 
 const router = Router();
 
 router.use(rootRouter);
+router.use(authRouter);
+router.use(userRouter);
 
 export default router;
