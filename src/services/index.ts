f54c74e296efb641075919cc6f57c9aa6fa25ab9
anon-kae <anon.ksl42@gmail.com>
2024-06-01 16:39:51 +0700

    feat: Add AuthenticationService and update services index

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,15 +1,22 @@
 import * as repositories from '@/repositories';
 import UserService from './UserService';
+import AuthenticationService from './AuthenticationService';
 
 // Inject repositories of each service into the service
 const userService = new UserService(repositories.blog);
+const authenticationService = new AuthenticationService(repositories.blog);
 
 const services = {
   UserService: userService,
+  AuthenticationService: authenticationService,
 };
 
 Object.values(services).forEach(service => {
   service.injectServices(services);
 });
 
-export {services as default, userService as UserService};
+export {
+  services as default,
+  userService as UserService,
+  authenticationService as AuthenticationService,
+};
