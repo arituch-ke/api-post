4aa8a0bc2d150a8cf3036f9698628ce4c9c34a75
anon-kae <anon.ksl42@gmail.com>
2024-06-01 22:29:42 +0700

    feat: Add PostRepository to blog service

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -1,12 +1,13 @@
 import * as databases from '@/modes';
 import UserRepository from './UserRepository';
+import PostRepository from './PostRepository';
 
 /*
  * Export repositories for each service, a service represents a microservice in the future
  */
-
 export const blog = {
   UserRepository,
+  PostRepository,
   transaction: databases.blog.sequelize.transaction.bind(
     databases.blog.sequelize
   ),
