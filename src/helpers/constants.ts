1462271c565da6162b3dfb618e2437034cdf6e27
anon-kae <anon.ksl42@gmail.com>
2024-06-01 16:37:13 +0700

    feat: default lifetime of access token , refresh token

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -5,3 +5,6 @@ export const SUPPORTED_IMAGE_FORMATS = [
   'image/webp',
   'image/bmp',
 ];
+
+export const DEFAULT_ACCESS_TOKEN_LIFETIME: string = '10m';
+export const DEFAULT_REFRESH_TOKEN_LIFETIME: string = '1d';
